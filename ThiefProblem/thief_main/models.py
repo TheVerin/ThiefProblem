12e1c471d0e7b0c2221636e4825aa7ef993e961e
TheVerin <tomekbeben95@gmail.com>
2018-12-28 11:23:24 +0100

    Database is added

--- ThiefProblem/thief_main/models.py
+++ ThiefProblem/thief_main/models.py
@@ -1,2 +1,18 @@
 from django.db import models
 
+
+class Thief(models.Model):
+    thief_name = models.CharField(max_length=50)
+    name_1 = models.CharField(max_length=50)
+    name_2 = models.CharField(max_length=50)
+    mass_1 = models.IntegerField()
+    mass_2 = models.IntegerField()
+    prize_1 = models.IntegerField()
+    prize_2 = models.IntegerField()
+    car_load = models.IntegerField()
+
+    class Meta:
+        db_table = 'thief'
+
+    def __str__(self):
+        return self.thief_name
