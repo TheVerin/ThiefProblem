379c439d613a8c2cca104bbb21bd22e51516f182
TheVerin <tomekbeben95@gmail.com>
2018-12-28 14:09:55 +0100

    Full working app

--- ThiefProblem/thief_main/views.py
+++ ThiefProblem/thief_main/views.py
@@ -29,7 +29,9 @@ def final(request):
     prize_1 = 1
     prize_2 = 1
     car_load = 10
-    result = 'here will be your solution :)'
+    res = 1
+    num_a = 0
+    num_b = 0
 
     if request.GET.get('thief_name'):
         thief_name = request.GET.get('thief_name')
@@ -47,17 +49,20 @@ def final(request):
         car_load = int(car_load_x)
 
         a = mass_checker(mass_1, mass_2, car_load)
-        result = profit_counter(a, prize_1, prize_2)
+        b = profit_counter(a, prize_1, prize_2)
+        num_a = b['NumberOfA']
+        num_b = b['NumberOfB']
+        res = b['Profit']
 
     obj = Thief.objects.create(
-        thief_name='thief_name',
-        name_1='name_1',
-        name_2='name_2',
-        mass_1='mass_1',
-        mass_2='mass_2',
-        prize_1='prize_1',
-        prize_2='prize_2',
-        car_load='car_load')
+        thief_name=thief_name,
+        name_1=name_1,
+        name_2=name_2,
+        mass_1=mass_1,
+        mass_2=mass_2,
+        prize_1=prize_1,
+        prize_2=prize_2,
+        car_load=car_load,)
     obj.save()
 
     return render(
@@ -71,6 +76,9 @@ def final(request):
             'mass_2': mass_2,
             'prize_1': prize_1,
             'prize_2': prize_2,
-            'car_load': car_load
+            'car_load': car_load,
+            'res': res,
+            'num_a': num_a,
+            'num_b': num_b,
         }
     )
